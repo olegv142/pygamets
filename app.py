20b95ec1ac70db8ea5f1b46792288f3c0576b36b
Oleg Volkovwq <olegv142@gmail.com>
2018-01-01 21:48:30 +0000

    Timers and jobs added to the application class

--- app.py
+++ app.py
@@ -1,39 +1,129 @@
+"""
+The module provides input events filter for pygame to ensure
+correct handling of the touch-screen events. In addition it
+provides several useful capabilities:
+  - callbacks called in the context of the event loop
+  - asynchronous jobs called once in the context of the event loop
+    (for foreign threads safe interfacing)
+  - timers called in the context of the event loop
+Note that all above facilities are available regardless of the
+particular event loop implementation. So this gives rather flexible
+approach to extending existing code capabilities.
+"""
+
 import os
+import threading
 import pygame as pg
 from events import read_events
 import calibration
 
-# Stop reading events by pygame engine to avoid erratic mouse pointer behaviour
-os.putenv('SDL_MOUSEDEV', '/dev/null')
+# Application instance
+instance = None
+
+def init():
+	"""Create application instance singleton"""
+	if instance is not None:
+		return instance
+	return Application()
+
+class Timer(object):
+	"""Timer object to be called in the context of the event loop"""
+	def __init__(self, cb, interval, periodic):
+		self.cb = cb
+		self.interval = interval
+		self.periodic = periodic
+
+	def cancel(self):
+		"""Cancel timer so it wont be fired anymore"""
+		self.cb = None
 
 class Application(object):
 
 	def __init__(self):
-		self.mouse_pos = None
-		self.mouse_down = False
-		self._down = None
+		global instance
+		assert instance is None
+		self.pygame_init()
 		inf = pg.display.Info()
 		self.screen_w = inf.current_w
 		self.screen_h = inf.current_h
+		self.mouse_pos = None
+		self.mouse_down = False
+		self._down = None
 		self.calib = calibration.load()
-		self.pygame_get_events = pg.event.get
-		self.event_loop_callback = None
-		pg.event.get = self.get_events
+		self.event_loop_callbacks = []
+		self.job_lock = threading.Lock()
+		self.job_list = []
+		self.timers = []
+		instance = self
+
+	def pygame_init(self):
+		"""Proper initialize pygame module"""
+		# stop reading events by pygame engine to avoid erratic mouse pointer behaviour
+		os.putenv('SDL_MOUSEDEV', '/dev/null')
+		pg.init()
+		pg.display.init()
+		pg.display.set_mode()
 		pg.event.set_blocked((pg.MOUSEBUTTONDOWN, pg.MOUSEBUTTONUP, pg.MOUSEMOTION))
+		# install event filter
+		self.pygame_get_events = pg.event.get
+		pg.event.get = self.get_events
 
-	def set_event_loop_callback(self, cb):
-		"""Set callback to be called on every event query. Returns previous callback."""
-		old_cb = self.event_loop_callback
-		self.event_loop_callback = cb
-		return old_cb
+	def add_event_loop_callback(self, cb):
+		"""Add callback to be called in event loop context"""
+		self.event_loop_callbacks.append(cb)
 
-	def raw_pos_to_screen(self, (x, y)):
-		return self.screen_w * x // 4096, self.screen_h * y // 4096
+	def run_callbacks(self):
+		"""Execute registered callbacks"""
+		for cb in self.event_loop_callbacks:
+			cb()
+
+	def add_job(self, job):
+		"""Add job to be called once in event loop context"""
+		with self.job_lock:
+			self.job_list.append(job)
+
+	def run_jobs(self):
+		"""Execute registered jobs"""
+		with self.job_lock:
+			self.job_list, job_list = [], self.job_list
+		for job in job_list:
+			job()
+
+	def add_timer(self, timer):
+		"""Add timer object"""
+		now = pg.time.get_ticks()
+		self.timers.append((now + timer.interval, timer))
+		self.timers.sort()
+
+	def process_timers(self):
+		"""Process registered timers"""
+		now = pg.time.get_ticks()
+		not_expired = len(self.timers)
+		for i, (t, _) in enumerate(self.timers):
+			if t > now:
+				not_expired = i
+				break
+		if not not_expired:
+			return
+		expired, self.timers = self.timers[:not_expired], self.timers[not_expired:]
+		rescheduled = False
+		for due, timer in expired:
+			if timer.cb:
+				timer.cb()
+			if timer.cb:
+				if timer.periodic:
+					self.timers.append((due + timer.interval, timer))
+					rescheduled = True
+				else:
+					timer.cb = None
+		if rescheduled:
+			self.timers.sort()
 
 	def get_events(self):
-		"""Query events. This is the replacement for the pygame.event.get"""
-		if self.event_loop_callback:
-			self.event_loop_callback()
+		"""Query events filter. This is the replacement for the pygame.event.get"""
+		self.run_callbacks()
+		self.run_jobs()
+		self.process_timers()
 
 		events = self.pygame_get_events()
 		sys_events = read_events()
--- test/app_event_test.py
+++ test/app_event_test.py
@@ -4,14 +4,30 @@ import sys, time
 import pygame as pg
 
 sys.path.append('..')
-from app import Application
+import app
 
-pg.display.init()
-pg.display.set_mode()
-app = Application()
+def print_event(e):
+	def fn():
+		print e
+	return fn
+
+app.init()
+cnt, last_cnt = 0, 0
+
+def loop_counter():
+	global cnt
+	cnt += 1
+
+def loop_timer():
+	global last_cnt
+	print cnt - last_cnt, 'loops per second'
+	last_cnt = cnt
+
+app.instance.add_event_loop_callback(loop_counter)
+app.instance.add_timer(app.Timer(loop_timer, 1000, periodic=True))
 
 while True:
-	time.sleep(.05)
+	time.sleep(.01)
 	for e in pg.event.get():
-		print e
+		app.instance.add_job(print_event(e))
 
