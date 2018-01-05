e70fe4887421970cc3df430f1163537dbac24dc0
Oleg Volkov <olegv@parallels.com>
2018-01-05 10:10:08 +0300

    Allow using Style with object without parent link

--- style.py
+++ style.py
@@ -17,17 +17,25 @@ class Style(object):
 		self._owner = None
 
 	def bind_owner(self, owner):
+		"""
+		Set reference to the owner object - typically the view instance.
+		We use owner owner class and parent link to lookup attribute defaults.
+		"""
 		self._owner = owner
 
 	def __getattribute__(self, key):
+		"""The method called on attribute resolution"""
 		try:
+			# resolve plain attributes like _attrs, _owner
 			return object.__getattribute__(self, key)
 		except AttributeError:
 			pass
 		try:
+			# lookup at attributes dictionary
 			return self._attrs[key]
 		except KeyError:
 			pass
+		# lookup default values in global registry
 		v = Style._lookup_default(self._owner, self._attrs.get('name', None), key)
 		# store lookup result to speed up subsequent lookups of the same key
 		self._attrs[key] = v
@@ -39,8 +47,12 @@ class Style(object):
 		assert obj is not None
 		# Lookup the most specific path first
 		if path:
-			if obj.parent:
-				v = Style._lookup_default(obj.parent, name, key, type(obj.parent).__name__ + '.' + path)
+			try:
+				parent = obj.parent
+			except AttributeError:
+				parent = None
+			if parent:
+				v = Style._lookup_default(parent, name, key, type(parent).__name__ + '.' + path)
 				if v is not None:
 					return v
 		else:
