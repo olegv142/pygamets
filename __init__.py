83e7521d01c81cee64ba64072c1779e198e3a824
Oleg Volkov <olegv@parallels.com>
2018-01-12 10:50:45 +0300

    Package init file

---
+++ __init__.py
@@ -0,0 +1,17 @@
+"""
+GUI framework for pygame with focus on touch-screen support without X-windows
+"""
+
+import app
+import gui
+import frame
+import button
+import label
+import battery
+import progress
+import list_view
+import log_view
+import plot
+import style
+import localize
+import utils
